853d048497d09353f8513941bfe2fdf33a1fa1b2
Deep Baidya <124801473+dbaidya811@users.noreply.github.com>
2025-05-30 16:05:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Serve frontend files
+app.use(express.static(__dirname + '/public')); // Your HTML in /public folder
+
+io.on('connection', socket => {
+    console.log('User connected:', socket.id);
+
+    socket.on('set username', username => {
+        socket.username = username || `User_${socket.id.slice(0, 5)}`;
+    });
+
+    socket.on('chat message', msg => {
+        io.emit('chat message', {
+            user: socket.username || `User_${socket.id.slice(0, 5)}`,
+            message: msg,
+            socketId: socket.id
+        });
+    });
+
+    socket.on('file upload', file => {
+        io.emit('file upload', {
+            user: socket.username || `User_${socket.id.slice(0, 5)}`,
+            ...file
+        });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
